fbd3e0413a9b0b59abd22fd329113146a4b50ef3
Tatiana Sapun <tatiana.sapun@gmail.com>
2022-01-25 20:00:53 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openedBracketsExample = [];
+  let closedBracketsExample = [];
+  bracketsConfig.forEach( brackets => {
+    openedBracketsExample.push(brackets[0]);
+    closedBracketsExample.push(brackets[1]);
+  });
+
+  function closeOpened(char) {
+    if (opened[opened.length - 1] === openedBracketsExample[closedBracketsExample.indexOf(char)]) opened.pop();
+  }
+
+  let opened = [];
+
+  for (let char of str) {
+    if (closedBracketsExample.includes(char) && opened.includes(openedBracketsExample[closedBracketsExample.indexOf(char)])) {
+      closeOpened(char);
+    } else if (openedBracketsExample.includes(char)) {
+      opened.push(char);
+    } else return false;
+  }
+
+  return (opened.length) ? false : true;
 }
